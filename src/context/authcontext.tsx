be2950063e2932b1281d249693a8a036cb2aea5c
levi melo <levicontat@outlook.com>
2021-07-17 22:28:01 -0300

    dealing with invalid token

--- src/context/authcontext.tsx
+++ src/context/authcontext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import Router from "next/router";
-import { setCookie, parseCookies } from "nookies";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 
 type User = {
   email: string;
@@ -25,6 +25,13 @@ interface AuthContextData {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+
+  Router.push("/");
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
@@ -33,11 +40,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const { "nextauth.token": token } = parseCookies();
 
     if (token) {
-      api.get("/me").then((response) => {
-        const { email, permissions, roles } = response.data;
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
 
-        setUser({ email, permissions, roles });
-      });
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
     }
   }, []);
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
+import { signOut } from "../context/authcontext";
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -71,8 +72,9 @@ api.interceptors.response.use(
           });
         });
       } else {
+        signOut();
       }
     }
-    return api;
+    return Promise.reject(error);
   }
 );
